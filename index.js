6595f1a5c073089ad3e73d3d56bd65e771267e0a
Esteban <cordobaesteban77@gmail.com>
2025-07-22 20:39:00 -0300

    creo el carrito de usuario

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(express.json())
 app.use(cors())
 
 app.use("/usuarios", require("./routes/usuarios.routes"))
+app.use("/carritos", require("./routes/carritos.routes"))
 
 app.listen(3001,() => {
     console.log("Servidor andando en el puerto", 3001)
