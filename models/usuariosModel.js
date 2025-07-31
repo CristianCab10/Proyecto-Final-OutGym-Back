9a3be26a8895a80561e969f1887963e4eca71dda
Esteban <cordobaesteban77@gmail.com>
2025-07-31 20:01:49 -0300

    corrijo el nombre de la propiedad rolUsuario

--- models/usuariosModel.js
+++ models/usuariosModel.js
@@ -11,7 +11,7 @@ const UsuarioSchema = new Schema({
         required: true,
         trim: true,
     },
-    rol:{
+    rolUsuario:{
         type: String,
         enum: ["usuario", "admin"],
         default: "usuario"
