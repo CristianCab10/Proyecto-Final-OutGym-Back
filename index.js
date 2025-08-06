8eac264642c3ae5bfade451e4a537296a96737bd
cristian <cristiancabre10@gmail.com>
2025-08-06 20:18:23 -0300

    se creo tabla de turnos para el admin con boton de cancelar y formulario de reserva de turno

--- index.js
+++ index.js
@@ -2,13 +2,23 @@ require("./db/config.db")
 const express = require("express")
 const app = express()
 const cors = require("cors")
+const bookingsRoutes = require('./routes/bookings');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 app.use(express.json())
 app.use(cors())
 
 app.use("/usuarios", require("./routes/usuarios.routes"))
 app.use("/carritos", require("./routes/carritos.routes"))
+app.use('/bookings', bookingsRoutes);
 
-app.listen(3001,() => {
-    console.log("Servidor andando en el puerto", 3001)
+mongoose.connect(process.env.MONGO_CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 })
+.then(() => {
+  console.log('Mongo conectado');
+  app.listen(3001, () => console.log('Servidor corriendo en http://localhost:3001'));
+})
+.catch(err => console.error('Error al conectar MongoDB:', err));
