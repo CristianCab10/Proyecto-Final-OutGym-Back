fc34dc1df947dc22b972cb62d968b333621fb906
Córdoba Esteban Augusto <cordobaesteban77@gmail.com>
2025-08-07 19:10:35 -0300

    cofig uri mongo

--- db/config.db.js
+++ db/config.db.js
@@ -1,6 +1,9 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGO_CONNECT)
+  .connect(process.env.MONGO_CONNECT, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
   .then(() => console.log("Base de datos conectada"))
   .catch((error) => console.log(error));
