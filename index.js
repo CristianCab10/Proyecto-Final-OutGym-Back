b2b1ec9acfdc545267b72ac34b2de10bdd889a65
cristian <cristiancabre10@gmail.com>
2025-08-26 23:29:53 -0300

    se creo modulo de recuperar password

--- index.js
+++ index.js
@@ -1,38 +1,28 @@
+require("dotenv").config();
 require("./db/config.db");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const morgan = require("morgan");
 const path = require("path");
-const bookingsRoutes = require('./routes/bookings');
-const mongoose = require('mongoose');
-require('dotenv').config();
-const productosRoutes = require("./routes/productos.routes");
-
-//middlewares
-
+const mongoose = require("mongoose");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("/public", express.static(path.join(__dirname, "public")));
-app.use(cors());
 app.use(morgan("dev"));
-
-app.use("/usuarios", require("./routes/usuarios.routes"))
-app.use("/carritos", require("./routes/carritos.routes"))
-app.use('/bookings', bookingsRoutes);
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use("/uploads", express.static("uploads"));
 
-
-mongoose.connect(process.env.MONGO_CONNECT, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-.then(() => {
-  console.log('Mongo conectado');
-  app.listen(3001, () => console.log('Servidor corriendo en http://localhost:3001'));
-})
-.catch(err => console.error('Error al conectar MongoDB:', err));
-app.use("/api/carrito", require("./routes/carritos.routes"));
+// Rutas
+app.use("/usuarios", require("./routes/usuarios.routes"));
+app.use("/carritos", require("./routes/carritos.routes"));
+app.use("/bookings", require("./routes/bookings"));
 app.use("/api/productos", require("./routes/productos.routes"));
 
+mongoose.connect(process.env.MONGO_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Mongo conectado");
+    app.listen(3001, () => console.log("Servidor corriendo en http://localhost:3001"));
+  })
+  .catch(err => console.error("Error al conectar MongoDB:", err));
